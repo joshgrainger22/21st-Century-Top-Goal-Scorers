aa1a1b73977c512c41a89681b34d916e6163b577
Joshua Grainger <Joshgrainger22@gmail.com>
2022-03-29 10:52:59 -0500

    added routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,17 @@ const { Router } = require('express')
 const controllers = require('../controllers')
 const router = Router()
 
+router.get('/', (req, res) => res.send('Home Page'))
 
+router.get('/bundesliga', controllers.getAllBundesligaPlayers)
+
+router.get('/bpl', controllers.getAllBplPlayers)
+
+router.get('/ligue1', controllers.getAllLigue1Players)
+
+router.get('/laliga', controllers.getAllLaLigaplayers)
+
+router.get('/topscorers', controllers.getAllTopScorers)
 
 
 
